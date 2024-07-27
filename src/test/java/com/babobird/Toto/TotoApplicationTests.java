da726c81825d92df59b611d8cd8464045feddff6
배세은 <baeseeun@baeseeun-ui-MacBookAir.local>
2024-07-27 18:05:13 +0900

    first commit

---
+++ src/test/java/com/babobird/Toto/TotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babobird.Toto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
