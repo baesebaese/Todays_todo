cb57c2d0d12840fe659c808731f3e3f980ea2561
cloud0448 <cloud00448@gmail.com>
2024-10-05 23:53:10 +0900

    파일명 변경, tasks 관련 파일 정리

---
+++ src/main/resources/static/js/inputForm.js
@@ -0,0 +1,27 @@
+const inputBox = document.getElementById("inputBox");
+const warningMessage = document.getElementById("warningMessage");
+
+// 초기 상태 설정
+inputBox.classList.remove('error');
+warningMessage.style.visibility = 'hidden';
+
+// input 중 자리수 초과시 알림
+inputBox.addEventListener('input', () => {
+  inputValue = inputBox.value;
+  if (inputValue.length > 10) {
+    inputBox.classList.add('error');
+    warningMessage.style.visibility = 'visible';
+  }
+  else {
+    inputBox.classList.remove('error');
+    warningMessage.style.visibility = 'hidden';
+  }
+});
+
+function submitValue() {
+      alert(inputValue)
+}
+
+function cancelInput() {
+    alert("창닫기")
+}
