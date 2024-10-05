9fd67f2dbe9820e40256b7148d540c53cec68054
cloud0448 <cloud00448@gmail.com>
2024-10-05 22:01:51 +0900

    html, css, js 파일 정리

--- src/main/resources/static/js/taskform.js
+++ src/main/resources/static/js/taskform.js
@@ -1,12 +1,22 @@
-const inputValue = document.getElementById("inputBox").value;
+const inputBox = document.getElementById("inputBox");
+const warningMessage = document.getElementById("warningMessage");
 
-window.onload = function() {
-  inputValue = '';
-}
+// 초기 상태 설정
+inputBox.classList.remove('error');
+warningMessage.style.visibility = 'hidden';
 
-
-
-
+// input 중 자리수 초과시 알림
+inputBox.addEventListener('input', () => {
+  inputValue = inputBox.value;
+  if (inputValue.length > 10) {
+    inputBox.classList.add('error');
+    warningMessage.style.visibility = 'visible';
+  }
+  else {
+    inputBox.classList.remove('error');
+    warningMessage.style.visibility = 'hidden';
+  }
+});
 
 function submitValue() {
       alert(inputValue)
