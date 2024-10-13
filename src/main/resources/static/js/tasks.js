bdeccf4b34deccdf8d9d118f2efd547eb8c00a3e
cloud0448 <cloud00448@gmail.com>
2024-10-13 23:42:22 +0900

    task 삭제 시 화면에서 처리

--- src/main/resources/static/js/tasks.js
+++ src/main/resources/static/js/tasks.js
@@ -1,3 +1,4 @@
+// 마우스 오버시 수정/삭제 버튼 표시
 document.querySelectorAll('.tr-tasknm, .tr-taskicons').forEach(item => {
     const taskRow = item.closest('tr');
     const closeIcon = taskRow.querySelector('.tr-taskicons #close-icon');
@@ -16,3 +17,26 @@ document.querySelectorAll('.tr-tasknm, .tr-taskicons').forEach(item => {
     item.addEventListener('mouseover', showIcons);
     item.addEventListener('mouseout', hideIcons);
 });
+
+// delete 버튼 클릭시 삭제 처리
+function deleteTask(element) {
+    const totoNo = element.getAttribute('data-toto-no');
+    const taskNo = element.getAttribute('data-task-no');
+
+    const url = `/totos/${totoNo}/tasks/${taskNo}`;
+
+    fetch(url, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Task deleted successfully!');
+        } else {
+            alert('Failed to delete the task.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while deleting the task.');
+    });
+}
